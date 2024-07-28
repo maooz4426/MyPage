4afb355960107539ed177c62d325052c5f7b5be8
MAOZ <maooz4426@gmail.com>
2024-07-28 20:13:07 +0900

    github等no

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,60 @@
-"use client"; //かかないと500エラー起きる
+"use client"; // 500エラーを防ぐために必要
 import Image from "next/image";
 import {AnimatePresence, motion} from 'framer-motion'
-import styles from "./page.module.css";
-
+import IconLink from "../components/iconLink";
 
 export default function Home() {
-  return (
-      // <div className="bg-gradient-to-b from-slate-900 to-slate-800  min-h-screen　w-full h-screen">
-      <div className={`min-h-screen`}>
-          <AnimatePresence mode="wait">
-              <motion.div
-                  className="flex my-10 items-center justify-center w-full h-full"
-                  initial={{opacity: 0}}
-                  animate={{opacity: 1}}
-                  transition={{duration: 0.5,delay: 0.5}}
-                  key="image-container"
-              >
-                  <div >
-                      <Image
-                          src="/images/img.png"
-                          alt="icon"
-                          width={500}
-                          height={500}
-                      />
-                  </div>
+    return (
+        <div className="min-h-screen">
+            <AnimatePresence mode="wait">
+                <motion.div
+                    className="flex my-10 items-center justify-center w-full h-full"
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 0.5,delay: 0.5}}
+                    key="image-container"
+                >
+                    <div>
+                        <Image
+                            src="/images/img.png"
+                            alt="icon"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                </motion.div>
+                <motion.div
+                    className="flex my-10 items-center justify-center w-full h-full"
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 1,delay: 1}}
+                    key="image-container"
+                >
+                    <div className="name text-center text-9xl">
+                        <h1>MAOZ</h1>
+                    </div>
+                </motion.div>
 
-              </motion.div>
-              <motion.div className="flex my-10 items-center justify-center w-full h-full"
-                  initial={{opacity: 0}}
-                  animate={{opacity: 1}}
-                  transition={{duration: 1,delay: 1}}
-                  key="image-container">
-                  <div className="name text-center text-9xl">
-                      <h1>MAOZ</h1>
-                  </div>
-              </motion.div>
-              <motion.div className={"flex my-10 items-center justify-center w-full h-full"}
-                          initial={{opacity: 0}}
-                          animate={{opacity: 1}}
-                          transition={{duration: 1,delay: 2}}>
-                      <Image src="/images/Github.svg"
-                             alt="Github logo"
-                             width={100}
-                             height={100}/>
-              </motion.div>
-
-
-          </AnimatePresence>
-
-      </div>
-  );
+                <motion.div
+                    className="flex my-10 items-center justify-center w-full h-full"
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 1, delay: 2}}
+                >
+                    <IconLink src="/images/Github.svg"
+                                   alt="Github logo"
+                                   href="https://github.com/maooz4426"
+                    />
+                    <IconLink src="/images/qiita-icon.png"
+                                   alt="qiita logo"
+                                   href="https://qiita.com/maooz4426"
+                    />
+                    <IconLink src="/images/x-logo.svg"
+                                   alt="xicon logo"
+                                   href="https://x.com/maotan426"
+                    />
+                </motion.div>
+            </AnimatePresence>
+        </div>
+    );
 }
